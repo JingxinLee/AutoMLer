6eccb03a91ffb3420cfa1999ae3f3739124e0a49
JingxinLee <zuibanglijingxin@gmail.com>
2023-12-16 21:14:01 +0800

    update

--- model_constructor/preprocess.py
+++ model_constructor/preprocess.py
@@ -16,6 +16,9 @@ import augly.audio as audaugs
 from augly.audio.utils import validate_and_load_audio
 from transformers import AutoTokenizer
 import augly.video as vidaugs
+import mimetypes
+
+
 
 def process_markdown_batch(markdown_files):
     batch_docs = []
@@ -486,3 +489,89 @@ def augment_video(aug_video):
         vidaugs.Compose([vidaugs.AddNoise(),vidaugs.Blur(sigma=5.0),vidaugs.OverlayDots(),])(video_path=aug_video, output_path=output_path)
     ]
     return random.choice(aug)
+
+####################### Task Inference#################################################
+# transformers version v4.36.1
+task_choices = ["AutoModelForCausalLM",
+                "AutoModelForMaskedLM",
+                "AutoModelForMaskGeneration",
+                "AutoModelForSeq2SeqLM",
+                "AutoModelForSequenceClassification",
+                "AutoModelForMultipleChoice",
+                "AutoModelForNextSentencePrediction",
+                "AutoModelForTokenClassification",
+                "AutoModelForQuestionAnswering",
+                "AutoModelForTextEncoding",
+                "AutoModelForDepthEstimation",
+                "AutoModelForlmageClassification",
+                "AutoModelForVideoClassification",
+                "AutoModelForMaskedImageModeling",
+                "AutoModelForObjectDetection",
+                "AutoModelForlmageSegmentation",
+                "AutoModelForImageTolmage",
+                "AutoModelForSemanticSegmentation",
+                "AutoModelForlnstanceSegmentation",
+                "AutoModelForUniversalSegmentation",
+                "AutoModelForZeroShotlmageClassification",
+                "AutoModelForZeroShotObjectDetection",
+                "AutoModelForAudioClassification",
+                "AutoModelForAudioFrameClassification",
+                "AutoModelForCTC",
+                "AutoModelForSpeechSeq2Seq",
+                "AutoModelForAudioXVector",
+                "AutoModelForTextToSpectrogram",
+                "AutoModelForTextToWaveform",
+                "AutoModelForTableQuestionAnswering",
+                "AutoModelForDocumentQuestionAnswering",
+                "AutoModelForVisualQuestionAnswering",
+                "AutoModelForVision2Seq",
+                ]
+
+
+def infer_task(file_path):
+    # 检测文件类型
+    mime_type, _ = mimetypes.guess_type(file_path)
+    
+    if mime_type is None:
+        return "Unknown file type"
+
+    # 文本文件
+    if mime_type.startswith('text'):
+        return ["AutoModelForCausalLM",
+                "AutoModelForMaskedLM",
+                "AutoModelForMaskGeneration",
+                "AutoModelForSeq2SeqLM",
+                "AutoModelForSequenceClassification",
+                "AutoModelForMultipleChoice",
+                "AutoModelForNextSentencePrediction",
+                "AutoModelForTokenClassification",
+                "AutoModelForQuestionAnswering",
+                "AutoModelForTextEncoding"]
+
+    # 图像文件
+    elif any(mime_type.startswith(t) for t in ['image']):
+        return ["AutoModelForDepthEstimation",
+                "AutoModelForlmageClassification",
+                "AutoModelForVideoClassification",
+                "AutoModelForMaskedImageModeling",
+                "AutoModelForObjectDetection",
+                "AutoModelForlmageSegmentation",
+                "AutoModelForImageTolmage",
+                "AutoModelForSemanticSegmentation",
+                "AutoModelForlnstanceSegmentation",
+                "AutoModelForUniversalSegmentation",
+                "AutoModelForZeroShotlmageClassification",
+                "AutoModelForZeroShotObjectDetection"]
+
+    # 音频文件
+    elif any(mime_type.startswith(t) for t in ['audio']):
+        return ["AutoModelForAudioClassification",
+                "AutoModelForAudioFrameClassification",
+                "AutoModelForCTC",
+                "AutoModelForSpeechSeq2Seq",
+                "AutoModelForAudioXVector",
+                "AutoModelForTextToSpectrogram",
+                "AutoModelForTextToWaveform"]
+
+    else:
+        return "Unsupported file type"
--- model_constructor/task_inference.py
+++ model_constructor/task_inference.py
@@ -1,3 +1,6 @@
+import os
+import mimetypes
+
 # transformers version v4.36.1
 task_choices = ["AutoModelForCausalLM",
                 "AutoModelForMaskedLM",
@@ -33,3 +36,52 @@ task_choices = ["AutoModelForCausalLM",
                 "AutoModelForVisualQuestionAnswering",
                 "AutoModelForVision2Seq",
                 ]
+
+
+def infer_task(file_path):
+    # 检测文件类型
+    mime_type, _ = mimetypes.guess_type(file_path)
+    
+    if mime_type is None:
+        return "Unknown file type"
+
+    # 文本文件
+    if mime_type.startswith('text'):
+        return ["AutoModelForCausalLM",
+                "AutoModelForMaskedLM",
+                "AutoModelForMaskGeneration",
+                "AutoModelForSeq2SeqLM",
+                "AutoModelForSequenceClassification",
+                "AutoModelForMultipleChoice",
+                "AutoModelForNextSentencePrediction",
+                "AutoModelForTokenClassification",
+                "AutoModelForQuestionAnswering",
+                "AutoModelForTextEncoding"]
+
+    # 图像文件
+    elif any(mime_type.startswith(t) for t in ['image']):
+        return ["AutoModelForDepthEstimation",
+                "AutoModelForlmageClassification",
+                "AutoModelForVideoClassification",
+                "AutoModelForMaskedImageModeling",
+                "AutoModelForObjectDetection",
+                "AutoModelForlmageSegmentation",
+                "AutoModelForImageTolmage",
+                "AutoModelForSemanticSegmentation",
+                "AutoModelForlnstanceSegmentation",
+                "AutoModelForUniversalSegmentation",
+                "AutoModelForZeroShotlmageClassification",
+                "AutoModelForZeroShotObjectDetection"]
+
+    # 音频文件
+    elif any(mime_type.startswith(t) for t in ['audio']):
+        return ["AutoModelForAudioClassification",
+                "AutoModelForAudioFrameClassification",
+                "AutoModelForCTC",
+                "AutoModelForSpeechSeq2Seq",
+                "AutoModelForAudioXVector",
+                "AutoModelForTextToSpectrogram",
+                "AutoModelForTextToWaveform"]
+
+    else:
+        return "Unsupported file type"
